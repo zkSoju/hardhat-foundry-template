d271bac642ae24ac2c893729e9b6c98e6a8eea30
soju <underrated@gmail.com>
2022-06-06 14:58:25 -0700

    refactor: remove old forge-std

---
+++ test/Token.s.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+
+import "../src/Token.sol";
+
+contract TokenTest is Test {
+    Token t;
+
+    function run() public {
+      vm.startBroadcast();
+
+        t = new Token();
+
+        vm.stopBroadcast();
+    }
+
+}
